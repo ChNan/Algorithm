fc4c02cc3927506f3707651a2ecab24a6556e920
FangWeiQuan <987266515@qq.com>
2015-02-27 18:17:47 +0800

    Quick sorting

---
+++ QuickSort.java
@@ -0,0 +1,30 @@
+public class QuickSort{
+	
+	public final static int[] array = new int []{11,4,34,3};
+
+
+	public static void main(String[] args){
+		sort(0,array.length-1);
+	}
+	
+
+  	void sort(int prevIndex,int afterIndex){
+
+  		int currentValue=array[prevIndex];
+
+  		while(prevIndex!=terIndex){
+
+			
+  		}
+  		sort();
+  		sort();
+  	}
+
+	
+
+	void swap(int great,int less){
+		int tmp = great;
+		great=less;
+		less =tmp;
+	}
+}
